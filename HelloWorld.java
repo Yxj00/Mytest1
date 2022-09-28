b04c0800e6a5211b0d355f2bc4f6bfc350fc26a2
Yxj00 <108531356+Yxj00@users.noreply.github.com>
2022-09-28 19:24:14 +0800

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,20 @@
+package com.maya;
+
+public class HelloWorld {
+    public static void main(String []args){
+        System.out.println("您好，余玄机");
+        int a=100;
+        System.out.println(a);
+        System.out.println("--------------");
+        short b=50;
+        System.out.println(b);
+        System.out.println("--------------");
+        char c='男';
+        System.out.println(c);
+        boolean d=true;
+        System.out.println(d);
+        //定义变量 3.145默认情况下是double类型，要想变成float类型需在数字后面加f，或者前面加(float)强制转换
+        float e= (float) 3.145;
+        System.out.println(e);
+    }
+}
