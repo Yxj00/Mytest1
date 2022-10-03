0c5b2aca273563132eb0154c1aa88835565c07a3
Yxj00 <108531356+Yxj00@users.noreply.github.com>
2022-10-03 21:57:34 +0800

    Add files via upload

---
+++ Text8.java
@@ -0,0 +1,14 @@
+package Tjise.daxue;
+
+/**
+ * idea断点调试如何设置条件
+ */
+public class Text8 {
+    public static void main(String[] args) {
+
+        for (int i=0;i<10;i++){
+            //在断点调试情况下，只想看到i==8，断点出鼠标右键
+            System.out.println(i);
+        }
+    }
+}
