192945ce0c691870a3d8e260987b1b98ede1a2e7
Yxj00 <108531356+Yxj00@users.noreply.github.com>
2023-01-03 21:35:56 +0800

    Add files via upload

---
+++ Instanceof.java
@@ -0,0 +1,9 @@
+package com.duixiang.day10;
+
+/**
+ * 对象的引用 instanceof 具体的类型(类或者接口) 可以是超类object 父类  自身类 子类
+ * instanceof 布尔类型
+ */
+public class Instanceof {
+
+}
--- Student.java
+++ Student.java
@@ -1,7 +1,42 @@
-package com.duixiang.day02;
+package com.duixiang.day10;
 
-public class Student extends Parent{
-//    public void a1(){
-//
-//    }
+public class Student {
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        //如果两个对象的内存地址相同直接返回true
+        if (this==obj){
+            return true;
+        }
+       //比较两个对象的类型是否一致 对象名.getclass()
+        if (obj==null||this.getClass()!=obj.getClass()){//两个对象类型不一致 则返回true
+            return false;
+        }
+        //两个对象的类型是一致的 则比较两个对象的成员属性值
+        //string类型中equals方法比较 两个字符串的值相等   String底层重写了object父类
+        return this.age==((Student) obj).age&&this.name.equals(((Student) obj).name);
+    }
 }
