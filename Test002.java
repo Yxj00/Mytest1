50deae3a64e4343f012f116b8e94e7ef14269e9e
Yxj00 <108531356+Yxj00@users.noreply.github.com>
2023-01-06 22:10:44 +0800

    Add files via upload

---
+++ Test002.java
@@ -0,0 +1,9 @@
+package com.duixiang.day11;
+
+public class Test002 {
+    public static void main(String[] args) {
+        String str="YXJyxj";
+        System.out.println(str.toLowerCase());//将大写的字母全部转换成小写
+        System.out.println(str.toUpperCase());//将小写的字母全部转换成大写
+    }
+}
